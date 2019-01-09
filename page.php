092feacdf758434109b74132f89f2916fa769e22
Christopher Lee <chrislee@umn.edu>
2019-01-08 22:48:58 -0600

    testing clean up fixes

--- page.php
+++ page.php
@@ -6,9 +6,6 @@
     the_post(); ?>
 
       <div class="container">
-        <div class="nav_bg_page"></div>
-        <!-- <span class="page_title"><?php the_title(); ?></span> -->
-        <br>
         <?php the_content(); ?>
       </div>
 
